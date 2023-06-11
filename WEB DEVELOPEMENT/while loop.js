5dda3555b06545ed0f6dfebea3b1b1e3d089363c
o-akshihab-0 <136222327+o-akshihab-0@users.noreply.github.com>
2023-06-11 14:39:36 +0600

    Add files via upload

---
+++ WEB DEVELOPEMENT/while loop.js
@@ -0,0 +1,37 @@
+console.log("assalamualaikum");
+
+let i=0;
+let total=0
+
+// 1
+while (i<=10) {
+    total=total+i;
+    i++;
+console.log(i);
+}
+
+// 2
+
+while (i<=10) {
+    total=total+i;
+    i++;
+}
+console.log(total);
+
+// 3
+
+let number=10
+
+let Total=(number*(number+1))/2
+
+
+// https://chat.openai.com/share/2e04e403-4f3c-43d3-ad66-baeef69ec614
+
+
+
+
+
+
+
+
+
